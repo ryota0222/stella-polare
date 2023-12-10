90c25abb31115c654099e3f76ae13fcadc0e1a46
RyoTa <ryotanny@gmail.com>
2023-12-10 23:28:05 +0900

    feat: Update /api/user

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,15 +1,8 @@
-import { parse } from "path";
-
 import Axios, { AxiosRequestConfig } from "axios";
-import qs from "qs";
 
 const axiosConfig: AxiosRequestConfig<any> = {
   timeout: 30000,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  paramsSerializer: {
-    encode: parse as any,
-    serialize: (params) => qs.stringify(params, { arrayFormat: "brackets" }),
-  },
 };
 
 const client = Axios.create(axiosConfig);
--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,14 +1,8 @@
-export const firebaseConfig = {
-  type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TYPE,
-  project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
-  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
-  client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_ID,
-  auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_URI,
-  token_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_TOKEN_URI,
-  auth_provider_x509_cert_url:
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_AUTH_PROVIDER_URL,
-  client_x509_cert_url: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_CERT_URL,
-  universe_domain: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_UNIVERSE_DOMAIN,
+export const serviceAccount = {
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PROJECT_ID,
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_CLIENT_EMAIL,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY!.replace(
+    /\\n/g,
+    "\n"
+  ),
 };
