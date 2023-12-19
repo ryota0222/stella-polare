caf95825d22a3ccca665bda5d65b2e176b15884e
RyoTa <ryotanny@gmail.com>
2023-12-19 13:58:29 +0900

    feat: Add Linkify

---
+++ src/components/LinkfyWrapper.tsx
@@ -0,0 +1,20 @@
+import { Text } from "@mantine/core";
+import { PropsWithChildren, ReactNode, memo } from "react";
+import Linkify from "react-linkify";
+
+export const LinkifyWrapper = memo<PropsWithChildren>(({ children }) => {
+  const componentDecorator = (
+    href: string,
+    text: string,
+    key: number
+  ): ReactNode => (
+    <a href={href} key={key} target="_blank" rel="noopener noreferrer">
+      {text}
+    </a>
+  );
+  return (
+    <Linkify componentDecorator={componentDecorator}>
+      <Text style={{ whiteSpace: "pre-wrap" }}>{children}</Text>
+    </Linkify>
+  );
+});
